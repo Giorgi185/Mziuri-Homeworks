b1de0fd1dd79c3c539e3fc129a1c5a7b2aa562e1
Giorgi <ggogokhia2@gmail.com>
2026-02-05 16:55:19 +0400

    Add files via upload

---
+++ classwork1.py
@@ -0,0 +1,52 @@
+#1
+# file = open("file","w")
+# file.write("hawks")
+# file.close()
+
+#2
+# file = open("file","r")
+# count = 0
+# for i in file:
+#     for n in i:
+#        if True:
+#             count += 1
+#
+# print(count)
+
+#3
+#
+# file = open("file", "a")
+# file.write("beberto")
+# file.close()
+
+#4
+# file = open("file","r")
+# file1 = open("file1","w")
+#
+# for i in file:
+#     file1.write(i)
+
+#5
+# file = open("file","r")
+# file1= open("file1","r")
+# file2 = open("file2","w")
+# for i in file:
+#     file2.write(i)
+#     for b in file1:
+#         file2.write(b)
+
+#6
+# file = open("file","r+")
+# file1=file.read()
+# file1 = file1.upper()
+# file.write(file1)
+# print(file1)
+
+#7
+# while True:
+#     a = input("ahhaaam: ")
+#     if a == 0:
+#         print("ravi ra")
+#     else:
+#         file = open("file","w"+"\n")
+#         file.write(a)
